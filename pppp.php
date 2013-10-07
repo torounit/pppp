66e3cd556438042da101615109694ed64d23e468
Toro_Unit <mail@torounit.com>
2013-10-07 23:47:32 +0900

    翻訳を追加。

--- pppp.php
+++ pppp.php
@@ -29,14 +29,24 @@ Class PPPP {
 		register_uninstall_hook( __FILE__, array( __CLASS__, 'uninstall_hook') );
 	}
 
+
 	public function add_hooks() {
+		add_action( 'init', array( $this,'load_textdomain') );
 		add_action( "pre_get_posts", array($this,"pre_get_posts"));
-		add_action("admin_init", array($this,"update_option"), 10);
-		add_action("admin_init", array($this,"add_settings_fields"), 11);
+		add_action( "admin_init", array($this,"update_option"), 10);
+		add_action( "admin_init", array($this,"add_settings_section"), 11 );
+		add_action( "admin_init", array($this,"add_settings_fields"), 12 );
 	}
 
+
+	public function load_textdomain() {
+		load_plugin_textdomain('pppp',false, dirname(plugin_basename(__FILE__))."/language" );
+	}
+
+
+
 	private static function get_post_types() {
-		return get_post_types( array('_builtin'=>false, 'publicly_queryable'=>true, 'show_ui' => true), "objects");
+		return get_post_types( array('_builtin'=>false, 'publicly_queryable'=>true, 'show_ui' => true, "has_archive" => true), "objects");
 	}
 
 	private static function get_taxonomies() {
@@ -80,6 +90,15 @@ Class PPPP {
 		}
 	}
 
+	public function add_settings_section() {
+		add_settings_section('pppp', __("Powerful Posts Per Page",'pppp'), array( $this,'section_content'), 'reading');
+	}
+
+	public function section_content() {
+		?>
+		<p><?php _e("Set posts per page for each post type/taxonomy archives.", "pppp");?></p>
+		<?php
+	}
 
 	public function add_settings_fields() {
 		foreach (self::get_post_types() as $post_type) {
@@ -91,15 +110,15 @@ Class PPPP {
 	}
 
 	public function add_settings_field( $id, $obj, $type ) {
-			add_settings_field( $id, $obj->label, array($this, "create_field"), "reading", 'default', array("label_for" => $id, "obj" => $obj ,"type" => $type));
+			add_settings_field( $id, sprintf(__("%s archive show at most" ,"pppp"), $obj->label), array($this, "create_field"), "reading", 'pppp', array("label_for" => $id, "obj" => $obj ,"type" => $type));
 	}
 
 	public function create_field( $arg ) {
 		$obj = $arg["obj"];
-
 		$field = $arg["label_for"];
+		$value = esc_attr( get_option($field, $this->posts_per_page) );
 		?>
-		<input name="<?php echo $field;?>" type="number" step="1" min="-1" id="<?php echo $field;?>" value="<?php form_option( $field ); ?>" class="small-text" /> <?php _e( 'posts' ); ?>
+		<input name="<?php echo $field;?>" type="number" step="1" min="-1" id="<?php echo $field;?>" value="<?php echo $value; ?>" class="small-text" /> <?php _e( 'posts' ); ?>
 		<?php
 	}
 
