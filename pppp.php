dc919195c255701f4b94718490d01206254f4204
Toro-Unit <mail@torounit.com>
2013-11-04 16:53:16 +0900

    category tag fix.

--- pppp.php
+++ pppp.php
@@ -137,11 +137,15 @@ Class PPPP_Core implements PPPP_Module {
 	public function pre_get_posts( $query ) {
 		if($query->is_main_query() and !is_admin()) {
 			$posts_per_page = get_option( "posts_per_page" );
-			foreach (PPPP_Util::get_post_types() as $post_type) {
-				if($query->is_post_type_archive( $post_type->name )) {
-					$query->set("posts_per_page", get_option( "posts_per_page_of_cpt_".$post_type->name, $posts_per_page));
-					return;
-				}
+
+			if($query->is_category()) {
+				$query->set("posts_per_page", get_option( "posts_per_page_of_tax_category", $posts_per_page));
+				return;
+			}
+
+			if($query->is_tag()) {
+				$query->set("posts_per_page", get_option( "posts_per_page_of_tax_post_tag", $posts_per_page));
+				return;
 			}
 
 			foreach (PPPP_Util::get_taxonomies() as $taxonomy) {
@@ -150,6 +154,15 @@ Class PPPP_Core implements PPPP_Module {
 					return;
 				}
 			}
+
+			foreach (PPPP_Util::get_post_types() as $post_type) {
+				if($query->is_post_type_archive( $post_type->name )) {
+					$query->set("posts_per_page", get_option( "posts_per_page_of_cpt_".$post_type->name, $posts_per_page));
+					return;
+				}
+			}
+
+
 		}
 	}
 }
