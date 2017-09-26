42ca419e22300c7f377482040a0b33937938eb23
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-09-27 05:00:06 +0900

    fix phpcs. use retting API

--- PPPP/Module/Admin.php
+++ PPPP/Module/Admin.php
@@ -8,56 +8,79 @@
  * @since 0.7
  *
  */
-Class PPPP_Module_Admin extends PPPP_Module {
+class PPPP_Module_Admin extends PPPP_Module {
 
 	public function add_hook() {
-		add_action( "admin_init", array( $this, "add_settings_section" ), 11 );
-		add_action( "admin_init", array( $this, "add_settings_fields" ), 12 );
+		add_action( 'admin_init', array( $this, 'add_settings_section' ), 11 );
+		add_action( 'admin_init', array( $this, 'add_settings_fields' ), 12 );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_css_js' ) );
 		add_action( 'admin_footer', array( $this, 'pointer_js' ) );
 	}
 
 	public function add_settings_section() {
-		add_settings_section( 'pppp', __( "Powerful Posts Per Page", 'pppp' ), array(
-			$this,
-			'section_content'
-		), 'reading' );
+		add_settings_section(
+			'pppp',
+			__( 'Powerful Posts Per Page', 'pppp' ),
+			array( $this, 'section_content', ),
+			'reading'
+		);
 	}
 
 	public function section_content() {
-		?>
-		<p><?php _e( "Set posts per page for each post type/taxonomy archives.", "pppp" ); ?></p>
+		?><p><?php esc_html_e( 'Set posts per page for each post type/taxonomy archives.', 'pppp' ); ?></p>
 		<?php
 	}
 
 	public function add_settings_fields() {
 		foreach ( PPPP_Util::get_post_types() as $post_type ) {
-			$this->add_settings_field( "posts_per_page_of_cpt_" . $post_type->name, $post_type, "post_type" );
+			$this->add_settings_field( 'posts_per_page_of_cpt_' . $post_type->name, $post_type, 'post_type' );
 		}
 		foreach ( PPPP_Util::get_taxonomies() as $taxonomy ) {
-			$this->add_settings_field( "posts_per_page_of_tax_" . $taxonomy->name, $taxonomy, "taxonomy" );
+			$this->add_settings_field( 'posts_per_page_of_tax_' . $taxonomy->name, $taxonomy, 'taxonomy' );
 		}
 	}
 
 	private function add_settings_field( $id, $obj, $type ) {
-		add_settings_field( $id, sprintf( __( "%s archive show at most", "pppp" ), $obj->label ), array(
-			$this,
-			"create_field"
-		), "reading", 'pppp', array( "label_for" => $id, "obj" => $obj, "type" => $type ) );
+
+		add_settings_field(
+			$id,
+			/* translators: %s: Name of post type or taxonomy label */
+			sprintf( __( '%s archive show at most', 'pppp' ), $obj->label ),
+			array( $this, 'create_field', ),
+			'reading',
+			'pppp',
+			array(
+				'label_for' => $id,
+				'obj' => $obj,
+				'type' => $type
+			)
+		);
+
+		register_setting(
+			'reading',
+			$id,
+			array( $this, 'sanitize' )
+		);
 	}
 
 	public function create_field( $arg ) {
-		$obj = $arg["obj"];
-		$field = $arg["label_for"];
+		$obj   = $arg['obj'];
+		$field = $arg['label_for'];
 		$value = get_option( $field );
-		if ( $value === false ) {
-			$value = get_option( "posts_per_page" );
+		if ( false === $value ) {
+			$value = get_option( 'posts_per_page' );
 		}
 		?>
-		<input name="<?php echo esc_attr( $field ); ?>" type="number" step="1" min="-1"
-		       id="<?php echo esc_attr( $field ); ?>" value="<?php echo esc_attr( $value ); ?>"
-		       class="small-text"/> <?php _e( 'posts' ); ?>
+		<input name="<?php echo esc_attr( $field ); ?>" type="number" step="1" min="-1" id="<?php echo esc_attr( $field ); ?>" value="<?php echo esc_attr( $value ); ?>" class="small-text"/> <?php esc_html_e( 'posts' ); ?>
 		<?php
+	}
+
+	public function sanitize( $maybeint ) {
+		$value = intval( $maybeint );
+		if ( $value < - 1 ) {
+			return - 1;
+		}
+		return $value;
 	}
 
 
@@ -81,14 +104,14 @@ Class PPPP_Module_Admin extends PPPP_Module {
 	public function pointer_js() {
 		if ( ! is_network_admin() ) {
 			$dismissed = explode( ',', get_user_meta( get_current_user_id(), 'dismissed_wp_pointers', true ) );
-			if ( array_search( 'pppp_pointer072', $dismissed ) === false ) {
-				$content = "<h3>" . __( "Powerful Posts Per Page", 'pppp' ) . "</h3>" . __( "<p>From <a href='options-reading.php'>Reading</a>, set posts per page for each post type/taxonomy archives.</p>", "pppp" );
+			if ( array_search( 'pppp_pointer072', $dismissed, true ) === false ) {
+				$content = '<h3>' . __( 'Powerful Posts Per Page', 'pppp' ) . '</h3>' . __( "<p>From <a href='options-reading.php'>Reading</a>, set posts per page for each post type/taxonomy archives.</p>", 'pppp' );
 				?>
 				<script type="text/javascript">
 					jQuery(function ($) {
 
-						$("#menu-settings .wp-has-submenu").pointer({
-							content: "<?php echo $content;?>",
+						$('#menu-settings .wp-has-submenu').pointer({
+							content: "<?php echo wp_kses_post( $content ); ?>",
 							position: {"edge": "left", "align": "center"},
 							close: function () {
 								$.post('admin-ajax.php', {
--- PPPP/Module/Option.php
+++ PPPP/Module/Option.php
@@ -8,54 +8,21 @@
  * @since 0.7
  *
  */
-Class PPPP_Module_Option extends PPPP_Module {
+class PPPP_Module_Option extends PPPP_Module {
 
 	public function add_hook() {
-		add_action( "admin_init", array( $this, "save_option" ), 10 );
 		register_uninstall_hook( PPPP_PLUGIN_FILE, array( __CLASS__, 'uninstall_hook' ) );
 
 	}
 
-	public function save_option() {
-		if ( isset( $_POST['submit'] ) && isset( $_POST['_wp_http_referer'] ) && strpos( $_POST['_wp_http_referer'], 'options-reading.php' ) !== false ) {
-			self::update_all_options();
-		}
-	}
-
-	public static function update_all_options() {
-		foreach ( PPPP_Util::get_post_types() as $post_type ) {
-			if ( isset( $_POST[ "posts_per_page_of_cpt_" . $post_type->name ] ) ) {
-				self::update_option( "posts_per_page_of_cpt_" . $post_type->name, $_POST[ "posts_per_page_of_cpt_" . $post_type->name ] );
-			}
-		}
-
-		foreach ( PPPP_Util::get_taxonomies() as $taxonomy ) {
-			if ( isset( $_POST[ "posts_per_page_of_tax_" . $taxonomy->name ] ) ) {
-				self::update_option( "posts_per_page_of_tax_" . $taxonomy->name, $_POST[ "posts_per_page_of_tax_" . $taxonomy->name ] );
-			}
-		}
-	}
-
-	public static function delete_all_options() {
-		foreach ( PPPP_Util::get_post_types() as $post_type ) {
-			delete_option( "posts_per_page_of_cpt_" . $post_type->name );
-		}
-
-		foreach ( PPPP_Util::get_taxonomies() as $taxonomy ) {
-			delete_option( "posts_per_page_of_tax_" . $taxonomy->name );
-		}
-	}
-
-	public static function update_option( $key, $value ) {
-		$value = intval( $value );
-		if ( $value < - 1 ) {
-			$value = - 1;
-		}
-		update_option( $key, $value );
-	}
-
 	public static function uninstall_hook() {
-		self::delete_all_options();
+		foreach ( PPPP_Util::get_post_types() as $post_type ) {
+			delete_option( 'posts_per_page_of_cpt_' . $post_type->name );
+		}
+
+		foreach ( PPPP_Util::get_taxonomies() as $taxonomy ) {
+			delete_option( 'posts_per_page_of_tax_' . $taxonomy->name );
+		}
 	}
 
 }
